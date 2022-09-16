17b6d8152574b98c4c224d791e71d232e3a079f1
Zart Colwing <zartc@wanadoo.fr>
2022-09-16 12:01:52 +0200

    restore normal LCTRL,WIN,LALT order

--- keyboards/gmmk/pro/rev1/ansi/keymaps/zartc/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/zartc/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------+------+------+------+------+------+------+------+------+------+------+------|----++======++------|
      * |  LShift      |  Z   |  X   |  C   |  V   |  B   |  N   |  M   | , <  | . >  | / ?  | RShift ||  Up  || End  |
      * |--------------+------+------+------+------+------+------+------+------+------+------+-++=====++------++======|
-     * |   Fn   |  LAlt  | LCtrl |                Space                  | RAlt | RCtrl| Win  || Left | Down | Right |
+     * | LCtrl |  Win  |  LAlt |                  Space                   | RAlt | RCtrl | Fn || Left | Down | Right |
      * '------------------------------------------------------------------------------------------------------------'
      */
     [BASE] = LAYOUT(
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,            KC_HOME,
         KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,            KC_PGUP,
         MO(MODE), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,             KC_PGDN,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,   KC_UP,   KC_END,
-        MO(FN), KC_LALT,  KC_LCTL,                            KC_SPC,                             KC_RALT, KC_RCTL, KC_LGUI,  KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,   KC_UP ,  KC_END,
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RCTL, MO(FN),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     /* MODE Layout
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------+------+------+------+------+------+------+------+------+------+------+------|----++======++------|
      * |  LShift      |Ctrl+Z|Ctrl+X|Ctrl+C|Ctrl+V|      | PgDn |BkSpc | Del  |      |      | RShift ||      ||      |
      * |--------------+------+------+------+------+------+------+------+------+------+------+-++=====++------++======|
-     * |   Fn   |  LAlt  | LCtrl |                Space                  | RAlt | RCtrl| Win  || Left | Down | Right |
+     * | LCtrl |  Win  |  LAlt |                  Space                   | RAlt | RCtrl | Fn || Left | Down | Right |
      * '------------------------------------------------------------------------------------------------------------'
      */
     [MODE] = LAYOUT(
@@ -80,11 +80,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NUM] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,               _______,
     //  ----------------------------------------------------------------------------------------------------------------------------------------------------------
-        TO(BASE), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PMNS, KC_PPLS, _______,               _______,
-        _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   XXXXXXX, S(KC_9), S(KC_0), KC_PEQL,               _______,
-        _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PAST, XXXXXXX,          KC_PENT,               _______,
-        _______,           C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX, XXXXXXX, KC_P0,   KC_COMM, KC_PDOT, KC_PSLS,          _______,      _______, _______,
-        _______,  _______, _______,                            _______,                            _______, _______, _______,      _______, _______, _______
+        TO(BASE),KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PMNS, KC_PPLS, _______,               _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   XXXXXXX, S(KC_9), S(KC_0), KC_PEQL,               _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PAST, XXXXXXX,          KC_PENT,               _______,
+        _______,          C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX, XXXXXXX, KC_P0,   KC_COMM, KC_PDOT, KC_PSLS,          _______,      _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______,      _______, _______, _______
     ),
 
 
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------+------+------+------+------+------+------+------+------+------+------+------|----++======++------|
      * |  LShift      |      |RgbTog|      |      |Q_Boot|NkTogg|      |      |      |      | RShift ||MedPrv|| Play |
      * |--------------+------+------+------+------+------+------+------+------+------+------+-++=====++------++======|
-     * |   Fn   |  LAlt  | LCtrl |                Space                  | RAlt | RCtrl| Win  ||MedPrv|MedNxt|MedNxt |
+     * | LCtrl |  Win  |  LAlt |                  Space                   | RAlt | RCtrl | Fn ||MedPrv|MedNxt|MedNxt|
      * '------------------------------------------------------------------------------------------------------------'
      */
     [FN] = LAYOUT(
